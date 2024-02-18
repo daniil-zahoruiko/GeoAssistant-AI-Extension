fde3eac07b4a26e4c1be4ee372cd3bb7f057ae77
Daniil Zahoruiko <daniel.zagoruyko@gmail.com>
2024-02-18 03:04:41 -0700

    Updated get canvas for webgl

--- source/scripts/getCanvas.js
+++ source/scripts/getCanvas.js
@@ -1,6 +1,19 @@
-const canvas = document.getElementsByClassName("mapsConsumerUiSceneCoreScene__canvas widget-scene-canvas");
 
-console.log(canvas[0].toDataURL());
+var canvas = document.getElementsByClassName("widget-scene-canvas")[0];
+var gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
 
-const img = document.getElementsByTagName("img");
-img[0].src = canvas[0].toDataURL();
+var tempCanvas = document.createElement('canvas');
+var tempCtx = tempCanvas.getContext('2d');
+
+tempCanvas.width = canvas.width;
+tempCanvas.height = canvas.height;
+
+tempCtx.drawImage(canvas, 0, 0);
+
+var imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+var pixels = imageData.data;
+console.log(pixels)
+// var image = new Image();
+// image.src = canvas[0].toDataURL()
+// var w = window.open("");
+// w.document.write(image.outerHTML);
