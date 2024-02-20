74e7c22c6d79a0e7bbb4fd91cd8684f4a192d0fb
Daniil Zahoruiko <daniel.zagoruyko@gmail.com>
2024-02-20 01:00:49 -0700

    Added request interception

---
+++ source/scripts/background.js
@@ -0,0 +1,12 @@
+const streetviewApiURL = "https://streetviewpixels-pa.googleapis.com/v1"
+
+function logURL(requestDetails) {
+    const url = requestDetails.url;
+    if(url.substring(0, url.lastIndexOf("/")) === streetviewApiURL){
+        console.log(`Loading: ${url}`);
+    }
+}
+  
+chrome.webRequest.onBeforeRequest.addListener(logURL, {
+    urls: ["https://www.geoguessr.com/*", "https://streetviewpixels-pa.googleapis.com/v1/*"],
+});
