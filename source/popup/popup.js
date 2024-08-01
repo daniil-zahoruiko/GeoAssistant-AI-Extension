2eceec9ed10ed435a5dd15d00abd0bc5e0ac65c3
Dmytro <da3e22@soton.ac.uk>
2024-08-01 21:03:04 +0100

    add storage functionality on popup

--- source/popup/popup.js
+++ source/popup/popup.js
@@ -13,3 +13,40 @@ document.addEventListener('DOMContentLoaded', (e) => {
             }
         });
 });
+
+
+var rectBox = document.querySelector('#rect');
+var dotBox = document.querySelector('#dot');
+
+rectBox.addEventListener('change', function() {
+    console.log(rectBox.checked);
+    chrome.storage.sync.set({preferences: {
+        rect: rectBox.checked,
+        dot: dotBox.checked
+    }});
+    // chrome.runtime.sendMessage({msg: 'preferencesChanged'});
+  });
+
+dotBox.addEventListener('change', function() {
+    console.log(dotBox.checked);
+    chrome.storage.sync.set({preferences: {
+        rect: rectBox.checked,
+        dot: dotBox.checked
+    }});
+});
+
+chrome.storage.sync.get('preferences', function(data) {
+    if (!data.preferences) {
+        chrome.storage.sync.set({preferences: {
+            rect: true,
+            dot: true
+        }});
+        rectBox.checked = true;
+        dotBox.checked = true;
+    } else {
+        chrome.storage.sync.get('preferences', function(data) {
+            rectBox.checked = data.preferences.rect;
+            dotBox.checked = data.preferences.dot;
+        });
+    }
+});
